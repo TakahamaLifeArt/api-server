953e870cd3fe28694921f5770fad22933ca920ec
Ks <ks.desk@gmail.com>
2018-02-21 22:32:04 +0900

    add クエリ実行メソッド

--- www/cgi-bin/package/db/SqlManager.php
+++ www/cgi-bin/package/db/SqlManager.php
@@ -1,7 +1,10 @@
 <?php
 /**
- * MySQLの接続とプリペアードステートメントの実行
- *
+ * MySQLの接続とクエリ及びプリペアードステートメントの実行
+ *--------------------
+ * log
+ * 2017-12-12 created
+ * 2018-02-20 execQueryを追加
  */
 declare(strict_types=1);
 namespace package\db;
@@ -9,7 +12,7 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/../cgi-bin/config.php';
 use \mysqli;
 use \Exception;
 class SqlManager {
-	private $_cn;
+	private $_cn;	// データベースサーバーへの接続を表すmysqliオブジェクト
 	
 	public function __construct() {
 		$this->dbConnect();
@@ -36,12 +39,40 @@ class SqlManager {
 	}
 	
 	/**
+	 * クエリーを実行する
+	 * @param {string} query
+	 * @return {array} selectクエリの結果セット、またはupdate, insert, deleteクエリで適用された行数
+	 */
+	public function execQuery(string $query): array {
+		try {
+			$result = $this->_cn->query($query);
+			$r = array();
+			
+			if ($result === FALSE) {
+//				$r[] = $this->_cn->errno.": ".$this->_conn->error;
+				throw new Exception();	// Error
+			} else if($result === TRUE) {
+				$r[] = $this->_cn->affected_rows;	// SELECT文以外
+			} else {
+				// select文
+//				$this->count = $result->num_rows;
+				while ($rec = $result->fetch_array(MYSQLI_BOTH)) {
+					$r[] = $rec;
+				}
+				$result->close();
+			}
+		} catch (Exception $e) {
+			$r = array();
+		}
+		return $r;
+	}
+	
+	/**
 	 * プリペアードステートメント
-	 * @param conn {mysqli} データベースサーバーへの接続を表すオブジェクト
-	 * @param {string} sql
+	 * @param {string} query
 	 * @param {string} marker
 	 * @param {array} param
-	 * @return {array} selectクエリの結果
+	 * @return {array} selectクエリの結果セット、またはupdate, insert, deleteクエリで適用された行数
 	 */
 	public function prepared(string $query, string $marker, array $param): array {
 		try {
@@ -52,9 +83,14 @@ class SqlManager {
 				$stmtParams[$key] =& $param[$key];	// bind_paramへの引数を参照渡しにする
 			}
 			call_user_func_array(array($stmt, 'bind_param'), $stmtParams);
-			$stmt->execute();
-			$stmt->store_result();
-			$r = $this->fetchAll($stmt);
+			if (!$stmt->execute()) throw new Exception();
+			$rows = $this->_cn->affected_rows;
+			if ($rows > 0) {
+				$r = array($rows);
+			} else {
+				$stmt->store_result();
+				$r = $this->fetchAll($stmt);
+			}
 			$stmt->close();
 			if (empty($r)) throw new Exception();
 		} catch (Exception $e) {
@@ -65,23 +101,29 @@ class SqlManager {
 	
 	/**
 	 * プリペアドステートメントから結果を取得し、バインド変数に格納する
-	 * @stmt	実行するプリペアドステートメントオブジェクト
-	 * @return	[カラム名:値, ...][]
+	 * @param stmt 実行するプリペアドステートメントオブジェクト
+	 * @return {array} [カラム名:値, ...][]
+	 * @throw 結果セットがない場合、空配列を返す
 	 */
-	protected function fetchAll( &$stmt) {
-		$hits = array();
-		$params = array();
-		$meta = $stmt->result_metadata();
-		while ($field = $meta->fetch_field()) {
-			$params[] =& $row[$field->name];
-		}
-		call_user_func_array(array($stmt, 'bind_result'), $params);
-		while ($stmt->fetch()) {
-			$c = array();
-			foreach($row as $key=>$val) {
-				$c[$key] = $val;
+	protected function fetchAll( &$stmt): array {
+		try {
+			$hits = array();
+			$params = array();
+			$meta = $stmt->result_metadata();
+			if (!is_object($meta)) throw new Exception();
+			while ($field = $meta->fetch_field()) {
+				$params[] =& $row[$field->name];
 			}
-			$hits[] = $c;
+			call_user_func_array(array($stmt, 'bind_result'), $params);
+			while ($stmt->fetch()) {
+				$c = array();
+				foreach($row as $key=>$val) {
+					$c[$key] = $val;
+				}
+				$hits[] = $c;
+			}
+		} catch (Exception $e) {
+			$hits = array();
 		}
 		return $hits;
 	}
