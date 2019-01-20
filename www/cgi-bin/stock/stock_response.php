25602f81e2bbc5d674d3584138859f2cf85be28d
Ks <ks.desk@gmail.com>
2019-01-20 22:13:35 +0900

    change トムス在庫データ更新処理

--- www/cgi-bin/stock/stock_response.php
+++ www/cgi-bin/stock/stock_response.php
@@ -1,9 +1,12 @@
 <?php
-/*
-*	トムス在庫確認メールの添付ファイルを読込みデータ抽出
-*	- postfixで受信したメールを受取り解析する
-*	log		: 2014-09-08 created
-*/
+/**
+ * トムス在庫確認メールの添付ファイルを読込みデータ抽出
+ * - postfixで受信したメールを受取り解析する
+ * log
+ * 2014-09-08 created
+ * 2016-03-16 在庫メールの商品名変更に伴う更新
+ * 2019-01-21 2XLとXXLのどちらかを3Lとする。混在している場合は2XLの在庫を使用する。
+ */
 
 require_once dirname(__FILE__).'/ReceiptMailDecoder.php';
 
@@ -36,55 +39,85 @@ $body = mb_convert_encoding($decoder->body['html'],"utf-8","jis");
 
 // マルチパートのデータを取得する
 setlocale(LC_ALL,'ja_JP.euc-jp');	// fgetcsv で全角文字を使用するため
+$curItem = null;
+$tmp = array();
 $data = array();
 if ( $decoder->isMultipart() ) {
 	$tempFiles = array();
 	$num_of_attaches = $decoder->getNumOfAttach();
 	for ( $i=0 ; $i < $num_of_attaches ; ++$i ) {
-		/*
-		* ファイルを一時ディレクトリ _TEMP_ATTACH_FILE_DIR_ に保存する
-		* 一時ファイルには tempnam()を使用する（パーミッション「0600」）
-		*/
-		//$fpath = tempnam( _TEMP_ATTACH_FILE_DIR_, "todoattach_" );
 		
+		// 一時ファイルにCSVを保存
 		$fpath = _TEMP_ATTACH_FILE_DIR_.'/'.$decoder->attachments[$i]['file_name'];
+		
 		if ( $decoder->saveAttachFile( $i, $fpath ) ) {
 			//$tempFiles["$fpath"] = $decoder->attachments[$i]['mime_type'];
 			chmod($fpath, 0604);
-			$pos = strpos(basename($fpath, '.csv'), 'wearzaiko');
-			if($pos!==false){
-				//ファイルを開く
-				if($fp = fopen($fpath, 'r')){
+			if (strpos(basename($fpath, '.csv'), 'wearzaiko') !== false) {
+				if ($fp = fopen($fpath, 'r')) {
 					$orders_id = 0;
 					$error_code = '';
 					$short = array();
 					fgetcsv($fp, 4096, _DELIMITER, _ENCLOSURE);	// 1行目を除外
 					while ($fld = fgetcsv($fp, 4096, _DELIMITER, _ENCLOSURE)) {
 						if(is_null($fld[0])) continue;	// 空行
+						
+						// アイテムのカラー毎に集計
+						if ($curItem !== $fld[1].'_'.$fld[3]) {
+							if (isset($tmp[$curItem])) {
+								foreach($tmp[$curItem] as $val) {
+									$data[] = $val;
+								}
+							}
+							$curItem = $fld[1].'_'.$fld[3];
+							$tmp = array();
+							$tmp[$curItem] = array();
+						}
+						
+						// Normalization
 						$sizename = mb_convert_kana($fld[6], 'a', 'sjis');	// 英数全角を半角に変換
 						$sizename = str_replace('cm', '', $sizename);
 						$sizename = mb_convert_encoding($sizename, 'utf-8', 'sjis');
+						
 						// サイズの呼称を統一する
-					    switch($sizename){
-					    	case 'フリー':
-							case 'F':			$sizename = 'Free';
-					    						break;
-					    	case 'LL':			$sizename = 'XL';
-					    						break;
-							case '2XL':			$sizename = '3L';
-					    						break;
-					    	case 'XXXL':		$sizename = '4L';
-					    						break;
-					    	case 'XXXXL':		$sizename = '5L';
-					    						break;
-					    	case 'XXXXXL':		$sizename = '6L';
-					    						break;
-					    	case 'XXXXXXL':		$sizename = '7L';
-					    						break;
-					    	case 'SS':			$sizename = 'XS';
-					    						break;
-					    }
-					    if($fld[1]==0){
+						switch ($sizename) {
+							case 'フリー':
+							case 'F':
+								$sizename = 'Free';
+								break;
+							case 'LL':
+								$sizename = 'XL';
+								break;
+							case 'XXL':
+								// 2XLとXXLが混在している場合は、2XLの在庫を使用する
+								if (isset($tmp[$curItem]['3L'])) {
+									$sizename = '';
+								} else {
+									$sizename = '3L';
+								}
+								break;
+							case '2XL':
+								$sizename = '3L';
+								break;
+							case 'XXXL':
+								$sizename = '4L';
+								break;
+							case 'XXXXL':
+								$sizename = '5L';
+								break;
+							case 'XXXXXL':
+								$sizename = '6L';
+								break;
+							case 'XXXXXXL':
+								$sizename = '7L';
+								break;
+							case 'SS':$sizename = 'XS';
+								break;
+						}
+
+						if ($sizename === '') continue;
+						
+						if($fld[1]==0){
 							$item_code = preg_replace('/^([a-z]{2})(\d{1,3})$/', '$1-$2', strtolower($fld[1]));		// du-001
 						}else if(strpos($fld[1], '0')===0){
 							/*
@@ -97,13 +130,21 @@ if ( $decoder->isMultipart() ) {
 						}else{
 							$item_code = sprintf("%03d", $fld[1]);		// 30016
 						}
-						$data[] = array(
-									'item_code'=>strtolower($item_code),
-									'color_code'=>$fld[3],
-									'size_name'=>$sizename,
-									'amount'=>$fld[8],
-									'jancode'=>$fld[9],
-								);
+						
+						// サイズ毎に一時配列で集計、サイズ名が重複している場合は上書き
+						$tmp[$curItem][$sizename] = array(
+							'item_code'=>strtolower($item_code),
+							'color_code'=>$fld[3],
+							'size_name'=>$sizename,
+							'amount'=>$fld[8],
+							'jancode'=>$fld[9],
+						);
+					}
+					
+					if (isset($tmp[$curItem])) {
+						foreach($tmp[$curItem] as $val) {
+							$data[] = $val;
+						}
 					}
 				}
 			}
--- www/cgi-bin/stock/tomsstock.php
+++ www/cgi-bin/stock/tomsstock.php
@@ -1,43 +1,38 @@
 <?php
-/*
-*	トムスの在庫確認メールの受信処理
-*	- postfixで受信したメールアカウントに連動して自動起動
-*	log		: 2014-09-08 created
-*/
+/**
+ * トムスの在庫確認メールを受信してデータベースを更新
+ * - postfixで受信したメールアカウントに連動して自動起動
+ * log
+ * 2014-09-08 created
+ * 2019-01-21 例外処理を更新
+ */
 
 require_once dirname(__FILE__).'/../mail/http.php';
 require_once dirname(__FILE__).'/../MYDB2.php';
 
 class TomsStock Extends MYDB2 {
 
-	public function __construct(){
-	}
+	public function __construct()
+	{}
 	
 	
 	/*
 	*	メール送信
 	*
 	*/
-	public function send($body){
+	public function send($body)
+	{
 		try{
-			
-			$attach = null;								// 添付ファイル情報
 			$email = array('test@takahama428.com');		// メールアドレス
-			$mail_subject = 'トムス在庫確認';			// 件名
+			$mail_subject = 'トムス在庫確認';				// 件名
+			$attach = null;								// 添付ファイル情報
 			
-			$mail_contents = $body;
 			// 本文
-			$mail_contents .= "トムス在庫確認\n\n";
-			$mail_contents .= "オリジナルＴシャツ屋　タカハマライフアート\n";
-			$mail_contents .= "〒124-0025　東京都葛飾区西新小岩3-14-26\n";
-			$mail_contents .= "（TEL）03-5670-0787\n";
-			$mail_contents .= "（FAX）03-5670-0730\n";
-			$mail_contents .= "E-mail："._INFO_EMAIL."\n";
-			$mail_contents .= "URL：http://www.takahama428.com\n";
-			
+			$mail_contents = "トムス在庫確認\n\n";
+			$mail_contents .= $body;
 			
 			// メール送信
-			$http = new HTTP('http://www.takahama428.com/v1/via_mailer.php');
+			$http = new HTTP('https://www.takahama428.com/v1/via_mailer.php');
 			$param = array(
 				'mail_subject'=>$mail_subject,
 				'mail_contents'=>$mail_contents,
@@ -46,11 +41,6 @@ class TomsStock Extends MYDB2 {
 			);
 			$res = $http->request('POST', $param);
 			
-			/*
-			$res = unserialize($res);
-			$reply = implode(',', $res);
-			*/
-
 		}catch (Exception $e) {
 			//$reply = 'ERROR: メールの送信が出来ませんでした。';
 		}
@@ -63,27 +53,26 @@ class TomsStock Extends MYDB2 {
 	*
 	*	@args		{'item_code','color_code','size_name','amount','jancode'}
 	*/
-	public function update_toms_stockdata($args){
-		try{
-			if(empty($args)) return;
+	public function update_toms_stockdata($args)
+	{
+		try {
+			if (empty($args)) return;
 			$rs = null;
 			$err = array();
 			$conn = parent::db_connect();
+			
 			// 在庫数を初期化
 			$update_time = date('Y-m-d H:i:s');
 			$curdate = date('Y-m-d');
-			/*
-			$sql = "update itemstock set stock_volume=0, stock_updated='".date('Y-m-d H:i:s')."'";
-			$result = $conn->query($sql);
-			if(!$result) throw new Exception('initialize');
-			*/
-			//$result->free();
+			
 			// 既存の在庫レコードの有無を確認
 			$sql1 = "select count(*) as cnt from itemstock where jan_code=? and stock_master_id=?";
 			$stmt_count = $conn->prepare($sql1);
+			
 			// 既存の在庫レコードがあれば更新
 			$sql2 = "update itemstock set stock_volume=?, stock_updated=? where jan_code=? limit 1";
 			$stmt_update = $conn->prepare($sql2);
+			
 			// 該当する登録データを取得
 			$sql3 = "select catalog.id as master_id, item.id as itemid, size_from from ((catalog";
 			$sql3 .= " inner join item on catalog.item_id=item.id)";
@@ -92,31 +81,35 @@ class TomsStock Extends MYDB2 {
 			$sql3 .= " where item_code like ? and color_code=? and size_name=? and maker_id=1";
 			$sql3 .= " and catalog.size_series=itemsize.series and catalogdate>? and itemdate>? and itemsizedate>?";
 			$stmt_choose = $conn->prepare($sql3);
+			
 			// 新規登録
 			$sql4 = "insert into itemstock(stock_master_id,stock_item_id,stock_size_id,stock_volume,stock_maker,jan_code,stock_updated) values(?,?,?,?,?,?,?)";
 			$stmt_insert = $conn->prepare($sql4);
+			
 			// 既存の在庫レコードの指定サイズの有無を確認
 			$sql5 = "select count(*) as cnt from itemstock where stock_size_id=? and stock_master_id=?";
 			$stmt_exists_size = $conn->prepare($sql5);
+			
 			// 在庫数を更新
-			for($i=0; $i<count($args); $i++){
+			for ($i=0; $i<count($args); $i++) {
 				$stmt_choose->bind_param("ssssss", $args[$i]['item_code'], $args[$i]['color_code'], $args[$i]['size_name'], $curdate, $curdate, $curdate);
 				$stmt_choose->execute();
 				$stmt_choose->store_result();
 				$rec = parent::fetchAll($stmt_choose);
-				for($t=0; $t<count($rec); $t++){
-					if(empty($rec[$t]['master_id'])){
+				for ($t=0; $t<count($rec); $t++) {
+					if (empty($rec[$t]['master_id'])) {
 						$err[] = $args[$i]['item_code'].' - '.$args[$i]['color_code'].' - '.$args[$i]['size_name'];
-					}else{
+					} else {
 						$stmt_count->bind_param("si", $args[$i]['jancode'],$rec[$t]['master_id']);
 						$stmt_count->execute();
 						$stmt_count->store_result();
 						$rec2 = parent::fetchAll($stmt_count);
-						if(empty($rec2[0]['cnt'])){
+						if (empty($rec2[0]['cnt'])) {
 							$stmt_exists_size->bind_param("ii", $args[$i]['size_from'],$rec[$t]['master_id']);
 							$stmt_exists_size->execute();
 							$stmt_exists_size->store_result();
 							$rec3 = parent::fetchAll($stmt_exists_size);
+							
 							// 新しいJANコードで且つ同じサイズが未登録の場合は新規登録
 							if (empty($rec3[0]['cnt'])) {
 								$dat = $rec[$t];
@@ -124,16 +117,16 @@ class TomsStock Extends MYDB2 {
 								$maker_id = 1;
 								$stmt_insert->execute();
 							}
-						}else{
+						} else {
 							$stmt_update->bind_param("iss", $args[$i]['amount'], date('Y-m-d H:i:s'), $args[$i]['jancode']);
 							$stmt_update->execute();
 						}
 					}
 				}
 			}
-			if(empty($err)){
-				$rs = 1;
-			}else{
+			if (empty($err)) {
+				$rs = '更新完了';
+			} else {
 				$rs = $err;
 			}
 			
@@ -141,11 +134,11 @@ class TomsStock Extends MYDB2 {
 			$sql = "delete from itemstock where stock_maker=1 and stock_updated<'".$update_time."'";
 			$result = $conn->query($sql);
 			if(!$result) throw new Exception('initialize');
-		}catch (Exception $e) {
-			if($e->getMessage()=='initialize'){
-				$rs = 9;
-			}else{
-				$rs = 2;
+		} catch (Exception $e) {
+			if ($e->getMessage()=='initialize') {
+				$rs = 'Error: Initialization.';
+			} else {
+				$rs = 'Error: Exception.';
 			}
 		}
 		$stmt_count->close();
@@ -171,9 +164,8 @@ if ( ($stdin=fopen("php://stdin",'r')) == true ){
 	}
 }
 */
-
 if ( STDIN == true ){		// stdinへのオープン済みストリーム
-	while( !feof(STDIN) ){
+	while ( !feof(STDIN) ) {
 		$raw_mail .= fgets(STDIN,4096);
 	}
 }
@@ -181,48 +173,34 @@ if ( STDIN == true ){		// stdinへのオープン済みストリーム
 require_once dirname(__FILE__).'/stock_response.php';
 
 $inst = new TomsStock();
-if(empty($data)){
+if (empty($data)) {
 	$args = '不明なメール: '.$decoder->attachments[0]['binary']."\n\n";
-}else{
+} else {
 	// データベース更新
 	$res = $inst->update_toms_stockdata($data);
 	
-	if(is_array($res)){
-		$args = "更新日時: ".date('Y-m-d H:i:s')."\n\n";
+	if (is_array($res)) {
+		$args = "更新完了: ".date('Y-m-d H:i:s')."\n\n";
 		$args .= "未更新アイテム\n";
 		for($i=0; $i<count($res); $i++){
 			$args .= "data: ".$res[$i]."\n";
 		}
-	}else{
-		switch($res){
-			case 1:		$args = "更新日時: ".date('Y-m-d H:i:s')."\n\n";
-						
-						/* DEBUG
-						for($i=2494; $i<2505; $i++){
-							$args .= "data: ".$data[$i]['jancode']."  -  ".$data[$i]['item_code']."  -  ".$data[$i]['color_code']."  -  ".$data[$i]['size_name']."\n";
-						}
-						*/
-						
-						break;
-			case 2:		$args = "Error: Exception.\n\n";
-						break;
-			case 3:		$args = "Error: No data.\n\n";
-						break;
-			case 9:		$args = "Error: Initialization.\n\n";
-						break;
-			default:	$args = "Error: Others.\n\n";
+	} else {
+		if (is_array($res)) {
+			$args = "更新完了: ".date('Y-m-d H:i:s')."\n\n";
+			$args .= "未更新アイテム\n";
+			for($i=0; $i<count($res); $i++){
+				$args .= "data: ".$res[$i]."\n";
+			}
+		} else if ($res === '更新完了') {
+			$args = $res.": ".date('Y-m-d H:i:s')."\n\n";
+		} else {
+			$args = $res."\n\n";
 		}
 	}
 }
 
-/* debug
-$start = 0;
-for($i=$start; $i<$start+10; $i++){
-	$args .= "data: ".$data[$i]['item_code']." - ".$data[$i]['color_code']." - ".$data[$i]['size_name']." - ".$data[$i]['amount']." - ".$data[$i]['jancode']."\n";
-}
-*/
-
 // 更新結果を送信
-//$inst->send($args);
+$inst->send($args);
 
 ?>
