22b20513238af7e965d35cef0b34a82105c6d798
Ks <ks.desk@gmail.com>
2019-05-09 19:59:10 +0900

    update ErrorDocument 404, 403 htaccess

---
+++ www/html/err/err404.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Not Found</title>
+	<style>
+		.container {
+			width: 100%;
+			max-width: 720px;
+			padding-right: 15px;
+			padding-left: 15px;
+			margin-right: auto;
+			margin-left: auto;
+			font-family: serif;
+		}
+		
+		h1 {
+			margin-bottom: 2rem;
+			padding: 2rem 0 0;
+			line-height: 1.2;
+			font-size: 2.25rem;
+			font-weight: bold;
+			color: rgb(7, 85, 123);
+		}
+		
+		p {
+			margin-top: 0;
+			margin-bottom: 1rem;
+		}
+	</style>
+</head>
+
+<body>
+
+	<div class="container">
+		<h1>404 Not Found</h1>
+		<p><strong>Error: 404</strong></p>
+		<p>The requested page does not exist.</p>
+	</div>
+
+</body>
+
+</html>
