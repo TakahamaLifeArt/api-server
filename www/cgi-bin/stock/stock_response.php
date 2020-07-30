cf2416a59a644f3745bad61cf5052b5e5cdbd7c1
Ks <ks.desk@gmail.com>
2020-07-30 23:12:41 +0900

    TLA トムスマスターの更新処理

--- www/cgi-bin/stock/stock_response.php
+++ www/cgi-bin/stock/stock_response.php
@@ -6,6 +6,7 @@
  * 2014-09-08 created
  * 2016-03-16 在庫メールの商品名変更に伴う更新
  * 2019-01-21 2XLとXXLのどちらかを3Lとする。混在している場合は2XLの在庫を使用する。
+ * 2020-07-30 tomsmasterテーブルの更新処理
  */
 
 require_once dirname(__FILE__).'/ReceiptMailDecoder.php';
@@ -38,118 +39,135 @@ $body = mb_convert_encoding($decoder->body['html'],"utf-8","jis");
 */
 
 // マルチパートのデータを取得する
-setlocale(LC_ALL,'ja_JP.euc-jp');	// fgetcsv で全角文字を使用するため
+setlocale(LC_ALL, 'ja_JP.euc-jp');	// fgetcsv で全角文字を使用するため
 $curItem = null;
 $tmp = array();
 $data = array();
-if ( $decoder->isMultipart() ) {
-	$tempFiles = array();
-	$num_of_attaches = $decoder->getNumOfAttach();
-	for ( $i=0 ; $i < $num_of_attaches ; ++$i ) {
-		
-		// 一時ファイルにCSVを保存
-		$fpath = _TEMP_ATTACH_FILE_DIR_.'/'.$decoder->attachments[$i]['file_name'];
-		
-		if ( $decoder->saveAttachFile( $i, $fpath ) ) {
-			//$tempFiles["$fpath"] = $decoder->attachments[$i]['mime_type'];
-			chmod($fpath, 0604);
-			if (strpos(basename($fpath, '.csv'), 'wearzaiko') !== false) {
-				if ($fp = fopen($fpath, 'r')) {
-					$orders_id = 0;
-					$error_code = '';
-					$short = array();
-					fgetcsv($fp, 4096, _DELIMITER, _ENCLOSURE);	// 1行目を除外
-					while ($fld = fgetcsv($fp, 4096, _DELIMITER, _ENCLOSURE)) {
-						if(is_null($fld[0])) continue;	// 空行
-						
-						// アイテムのカラー毎に集計
-						if ($curItem !== $fld[1].'_'.$fld[3]) {
-							if (isset($tmp[$curItem])) {
-								foreach($tmp[$curItem] as $val) {
-									$data[] = $val;
-								}
-							}
-							$curItem = $fld[1].'_'.$fld[3];
-							$tmp = array();
-							$tmp[$curItem] = array();
-						}
-						
-						// Normalization
-						$sizename = mb_convert_kana($fld[6], 'a', 'sjis');	// 英数全角を半角に変換
-						$sizename = str_replace('cm', '', $sizename);
-						$sizename = mb_convert_encoding($sizename, 'utf-8', 'sjis');
-						
-						// サイズの呼称を統一する
-						switch ($sizename) {
-							case 'フリー':
-							case 'F':
-								$sizename = 'Free';
-								break;
-							case 'LL':
-								$sizename = 'XL';
-								break;
-							case 'XXL':
-								// 2XLとXXLが混在している場合は、2XLの在庫を使用する
-								if (isset($tmp[$curItem]['3L'])) {
-									$sizename = '';
-								} else {
-									$sizename = '3L';
-								}
-								break;
-							case '2XL':
-								$sizename = '3L';
-								break;
-							case 'XXXL':
-								$sizename = '4L';
-								break;
-							case 'XXXXL':
-								$sizename = '5L';
-								break;
-							case 'XXXXXL':
-								$sizename = '6L';
-								break;
-							case 'XXXXXXL':
-								$sizename = '7L';
-								break;
-							case 'SS':$sizename = 'XS';
-								break;
-						}
+$tomsMaster = array();
 
-						if ($sizename === '') continue;
-						
-						if($fld[1]==0){
-							$item_code = preg_replace('/^([a-z]{2})(\d{1,3})$/', '$1-$2', strtolower($fld[1]));		// du-001
-						}else if(strpos($fld[1], '0')===0){
-							/*
-							 * 2016-03-16 在庫メールの商品名変更に伴い更新
-							$a = mb_convert_kana($fld[2], 'a', 'sjis');
-							preg_match("/^[a-zA-Z]+/", $a, $matches);
-							$item_code = sprintf("%03d", $fld[1])."-".substr($matches[0],0,3);	// 085-cvt
-							 */
-							$item_code = sprintf("%03d", $fld[1])."-%";	// 085-cvtは085-で前方一致
-						}else{
-							$item_code = sprintf("%03d", $fld[1]);		// 30016
-						}
-						
-						// サイズ毎に一時配列で集計、サイズ名が重複している場合は上書き
-						$tmp[$curItem][$sizename] = array(
-							'item_code'=>strtolower($item_code),
-							'color_code'=>$fld[3],
-							'size_name'=>$sizename,
-							'amount'=>$fld[8],
-							'jancode'=>$fld[9],
-						);
-					}
-					
-					if (isset($tmp[$curItem])) {
-						foreach($tmp[$curItem] as $val) {
-							$data[] = $val;
-						}
-					}
-				}
-			}
-		}
-	}
+if ($decoder->isMultipart()) {
+    $tempFiles = array();
+    $num_of_attaches = $decoder->getNumOfAttach();
+    for ($i=0 ; $i < $num_of_attaches ; ++$i) {
+        
+        // 一時ファイルにCSVを保存
+        $fpath = _TEMP_ATTACH_FILE_DIR_.'/'.$decoder->attachments[$i]['file_name'];
+        
+        if ($decoder->saveAttachFile($i, $fpath)) {
+            //$tempFiles["$fpath"] = $decoder->attachments[$i]['mime_type'];
+            chmod($fpath, 0604);
+            if (strpos(basename($fpath, '.csv'), 'wearzaiko') !== false) {
+                if ($fp = fopen($fpath, 'r')) {
+                    $orders_id = 0;
+                    $error_code = '';
+                    $short = array();
+                    fgetcsv($fp, 4096, _DELIMITER, _ENCLOSURE);	// 1行目を除外
+                    while ($fld = fgetcsv($fp, 4096, _DELIMITER, _ENCLOSURE)) {
+                        // 空行
+                        if (is_null($fld[0])) {
+                            continue;
+                        }
+
+                        // Normalization
+                        $sizename = mb_convert_kana($fld[6], 'a', 'sjis');	// 英数全角を半角に変換
+                        $sizename = str_replace('cm', '', $sizename);
+                        $sizename = mb_convert_encoding($sizename, 'utf-8', 'sjis');
+
+                        // tomsmaster更新用データ
+                        $tmp = array();
+                        $tmp['item_code'] = $fld[1];
+                        $tmp['item_name'] = $fld[2];
+                        $tmp['color_code'] = $fld[3];
+                        $tmp['color_name'] = $fld[4];
+                        $tmp['size_code'] = $fld[5];
+                        $tmp['size_name'] = $sizename;
+                        $tmp['jan_code'] = $fld[9];
+
+                        $tomsMaster[] = $tmp;
+
+                        // アイテムのカラー毎に集計
+                        if ($curItem !== $fld[1].'_'.$fld[3]) {
+                            if (isset($tmp[$curItem])) {
+                                foreach ($tmp[$curItem] as $val) {
+                                    $data[] = $val;
+                                }
+                            }
+                            $curItem = $fld[1].'_'.$fld[3];
+                            $tmp = array();
+                            $tmp[$curItem] = array();
+                        }
+
+                        // サイズの呼称を統一する
+                        switch ($sizename) {
+                            case 'フリー':
+                            case 'F':
+                                $sizename = 'Free';
+                                break;
+                            case 'LL':
+                                $sizename = 'XL';
+                                break;
+                            case 'XXL':
+                                // 2XLとXXLが混在している場合は、2XLの在庫を使用する
+                                if (isset($tmp[$curItem]['3L'])) {
+                                    $sizename = '';
+                                } else {
+                                    $sizename = '3L';
+                                }
+                                break;
+                            case '2XL':
+                                $sizename = '3L';
+                                break;
+                            case 'XXXL':
+                                $sizename = '4L';
+                                break;
+                            case 'XXXXL':
+                                $sizename = '5L';
+                                break;
+                            case 'XXXXXL':
+                                $sizename = '6L';
+                                break;
+                            case 'XXXXXXL':
+                                $sizename = '7L';
+                                break;
+                            case 'SS':$sizename = 'XS';
+                                break;
+                        }
+
+                        if ($sizename === '') {
+                            continue;
+                        }
+
+                        if ($fld[1]==0) {
+                            $item_code = preg_replace('/^([a-z]{2})(\d{1,3})$/', '$1-$2', strtolower($fld[1]));		// du-001
+                        } elseif (strpos($fld[1], '0')===0) {
+                            /*
+                             * 2016-03-16 在庫メールの商品名変更に伴い更新
+                            $a = mb_convert_kana($fld[2], 'a', 'sjis');
+                            preg_match("/^[a-zA-Z]+/", $a, $matches);
+                            $item_code = sprintf("%03d", $fld[1])."-".substr($matches[0],0,3);	// 085-cvt
+                             */
+                            $item_code = sprintf("%03d", $fld[1])."-%";	// 085-cvtは085-で前方一致
+                        } else {
+                            $item_code = sprintf("%03d", $fld[1]);		// 30016
+                        }
+
+                        // サイズ毎に一時配列で集計、サイズ名が重複している場合は上書き
+                        $tmp[$curItem][$sizename] = array(
+                            'item_code'=>strtolower($item_code),
+                            'color_code'=>$fld[3],
+                            'size_name'=>$sizename,
+                            'amount'=>$fld[8],
+                            'jancode'=>$fld[9],
+                        );
+                    }
+
+                    if (isset($tmp[$curItem])) {
+                        foreach ($tmp[$curItem] as $val) {
+                            $data[] = $val;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
-?>
--- www/cgi-bin/stock/tomsstock.php
+++ www/cgi-bin/stock/tomsstock.php
@@ -5,150 +5,222 @@
  * log
  * 2014-09-08 created
  * 2019-01-21 例外処理を更新
+ * 2020-07-30 tomsmasterテーブルの更新処理
  */
 
 require_once dirname(__FILE__).'/../mail/http.php';
 require_once dirname(__FILE__).'/../MYDB2.php';
 
-class TomsStock Extends MYDB2 {
+class TomsStock extends MYDB2
+{
+    public function __construct()
+    {
+    }
 
-	public function __construct()
-	{}
-	
-	
-	/*
-	*	メール送信
-	*
-	*/
-	public function send($body)
-	{
-		try{
-			$email = array('test@takahama428.com');		// メールアドレス
-			$mail_subject = 'トムス在庫確認';				// 件名
-			$attach = null;								// 添付ファイル情報
-			
-			// 本文
-			$mail_contents = "トムス在庫確認\n\n";
-			$mail_contents .= $body;
-			
-			// メール送信
-			$http = new HTTP('https://www.takahama428.com/v1/via_mailer.php');
-			$param = array(
-				'mail_subject'=>$mail_subject,
-				'mail_contents'=>$mail_contents,
-				'sendto'=>$email,
-				'reply'=>0
-			);
-			$res = $http->request('POST', $param);
-			
-		}catch (Exception $e) {
-			//$reply = 'ERROR: メールの送信が出来ませんでした。';
-		}
-	}
-	
-	
-	
-	/*
-	*	トムス商品の在庫情報を更新
-	*
-	*	@args		{'item_code','color_code','size_name','amount','jancode'}
-	*/
-	public function update_toms_stockdata($args)
-	{
-		try {
-			if (empty($args)) return;
-			$rs = null;
-			$err = array();
-			$conn = parent::db_connect();
-			
-			// 在庫数を初期化
-			$update_time = date('Y-m-d H:i:s');
-			$curdate = date('Y-m-d');
-			
-			// 既存の在庫レコードの有無を確認
-			$sql1 = "select count(*) as cnt from itemstock where jan_code=? and stock_master_id=?";
-			$stmt_count = $conn->prepare($sql1);
-			
-			// 既存の在庫レコードがあれば更新
-			$sql2 = "update itemstock set stock_volume=?, stock_updated=? where jan_code=? limit 1";
-			$stmt_update = $conn->prepare($sql2);
-			
-			// 該当する登録データを取得
-			$sql3 = "select catalog.id as master_id, item.id as itemid, size_from from ((catalog";
-			$sql3 .= " inner join item on catalog.item_id=item.id)";
-			$sql3 .= " inner join itemsize on item.id=itemsize.item_id)";
-			$sql3 .= " inner join size on size_from=size.id";
-			$sql3 .= " where item_code like ? and color_code=? and size_name=? and maker_id=1";
-			$sql3 .= " and catalog.size_series=itemsize.series and catalogdate>? and itemdate>? and itemsizedate>?";
-			$stmt_choose = $conn->prepare($sql3);
-			
-			// 新規登録
-			$sql4 = "insert into itemstock(stock_master_id,stock_item_id,stock_size_id,stock_volume,stock_maker,jan_code,stock_updated) values(?,?,?,?,?,?,?)";
-			$stmt_insert = $conn->prepare($sql4);
-			
-			// 既存の在庫レコードの指定サイズの有無を確認
-			$sql5 = "select count(*) as cnt from itemstock where stock_size_id=? and stock_master_id=?";
-			$stmt_exists_size = $conn->prepare($sql5);
-			
-			// 在庫数を更新
-			for ($i=0; $i<count($args); $i++) {
-				$stmt_choose->bind_param("ssssss", $args[$i]['item_code'], $args[$i]['color_code'], $args[$i]['size_name'], $curdate, $curdate, $curdate);
-				$stmt_choose->execute();
-				$stmt_choose->store_result();
-				$rec = parent::fetchAll($stmt_choose);
-				for ($t=0; $t<count($rec); $t++) {
-					if (empty($rec[$t]['master_id'])) {
-						$err[] = $args[$i]['item_code'].' - '.$args[$i]['color_code'].' - '.$args[$i]['size_name'];
-					} else {
-						$stmt_count->bind_param("si", $args[$i]['jancode'],$rec[$t]['master_id']);
-						$stmt_count->execute();
-						$stmt_count->store_result();
-						$rec2 = parent::fetchAll($stmt_count);
-						if (empty($rec2[0]['cnt'])) {
-							$stmt_exists_size->bind_param("ii", $args[$i]['size_from'],$rec[$t]['master_id']);
-							$stmt_exists_size->execute();
-							$stmt_exists_size->store_result();
-							$rec3 = parent::fetchAll($stmt_exists_size);
-							
-							// 新しいJANコードで且つ同じサイズが未登録の場合は新規登録
-							if (empty($rec3[0]['cnt'])) {
-								$dat = $rec[$t];
-								$stmt_insert->bind_param("iiiiiss", $dat['master_id'], $dat['itemid'], $dat['size_from'], $args[$i]['amount'], $maker_id, $args[$i]['jancode'], date('Y-m-d H:i:s'));
-								$maker_id = 1;
-								$stmt_insert->execute();
-							}
-						} else {
-							$stmt_update->bind_param("iss", $args[$i]['amount'], date('Y-m-d H:i:s'), $args[$i]['jancode']);
-							$stmt_update->execute();
-						}
-					}
-				}
-			}
-			if (empty($err)) {
-				$rs = '更新完了';
-			} else {
-				$rs = $err;
-			}
-			
-			// トムスの未更新アイテムを削除
-			$sql = "delete from itemstock where stock_maker=1 and stock_updated<'".$update_time."'";
-			$result = $conn->query($sql);
-			if(!$result) throw new Exception('initialize');
-		} catch (Exception $e) {
-			if ($e->getMessage()=='initialize') {
-				$rs = 'Error: Initialization.';
-			} else {
-				$rs = 'Error: Exception.';
-			}
-		}
-		$stmt_count->close();
-		$stmt_update->close();
-		$stmt_choose->close();
-		$stmt_insert->close();
-		$conn->close();
-		
-		return $rs;
-	}
+    /**
+     * メール送信
+     *
+     * @param string $body
+     * @return void
+     */
+    public function send($body)
+    {
+        try {
+            $email = array('test@takahama428.com');		// メールアドレス
+            $mail_subject = 'トムス在庫確認';				// 件名
+            $attach = null;								// 添付ファイル情報
+
+            // 本文
+            $mail_contents = "トムス在庫確認\n\n";
+            $mail_contents .= $body;
+
+            // メール送信
+            $http = new HTTP('https://www.takahama428.com/v1/via_mailer.php');
+            $param = array(
+                'mail_subject'=>$mail_subject,
+                'mail_contents'=>$mail_contents,
+                'sendto'=>$email,
+                'reply'=>0
+            );
+            $res = $http->request('POST', $param);
+        } catch (Exception $e) {
+            //$reply = 'ERROR: メールの送信が出来ませんでした。';
+        }
+    }
+
+    /**
+     * トムス商品の在庫情報を更新
+     *
+     * @param array args {'item_code','color_code','size_name','amount','jancode'}
+     * @return string|null
+     */
+    public function update_toms_stockdata($args)
+    {
+        try {
+            if (empty($args)) {
+                return;
+            }
+            $rs = null;
+            $err = array();
+            $conn = parent::db_connect();
+
+            // 在庫数を初期化
+            $update_time = date('Y-m-d H:i:s');
+            $curdate = date('Y-m-d');
+
+            // 既存の在庫レコードの有無を確認
+            $sql1 = "select count(*) as cnt from itemstock where jan_code=? and stock_master_id=?";
+            $stmt_count = $conn->prepare($sql1);
+
+            // 既存の在庫レコードがあれば更新
+            $sql2 = "update itemstock set stock_volume=?, stock_updated=? where jan_code=? limit 1";
+            $stmt_update = $conn->prepare($sql2);
+
+            // 該当する登録データを取得
+            $sql3 = "select catalog.id as master_id, item.id as itemid, size_from from ((catalog";
+            $sql3 .= " inner join item on catalog.item_id=item.id)";
+            $sql3 .= " inner join itemsize on item.id=itemsize.item_id)";
+            $sql3 .= " inner join size on size_from=size.id";
+            $sql3 .= " where item_code like ? and color_code=? and size_name=? and maker_id=1";
+            $sql3 .= " and catalog.size_series=itemsize.series and catalogdate>? and itemdate>? and itemsizedate>?";
+            $stmt_choose = $conn->prepare($sql3);
+
+            // 新規登録
+            $sql4 = "insert into itemstock(stock_master_id,stock_item_id,stock_size_id,stock_volume,stock_maker,jan_code,stock_updated) values(?,?,?,?,?,?,?)";
+            $stmt_insert = $conn->prepare($sql4);
+
+            // 既存の在庫レコードの指定サイズの有無を確認
+            $sql5 = "select count(*) as cnt from itemstock where stock_size_id=? and stock_master_id=?";
+            $stmt_exists_size = $conn->prepare($sql5);
+
+            // 在庫数を更新
+            for ($i=0; $i<count($args); $i++) {
+                $stmt_choose->bind_param("ssssss", $args[$i]['item_code'], $args[$i]['color_code'], $args[$i]['size_name'], $curdate, $curdate, $curdate);
+                $stmt_choose->execute();
+                $stmt_choose->store_result();
+                $rec = parent::fetchAll($stmt_choose);
+                for ($t=0; $t<count($rec); $t++) {
+                    if (empty($rec[$t]['master_id'])) {
+                        $err[] = $args[$i]['item_code'].' - '.$args[$i]['color_code'].' - '.$args[$i]['size_name'];
+                    } else {
+                        $stmt_count->bind_param("si", $args[$i]['jancode'], $rec[$t]['master_id']);
+                        $stmt_count->execute();
+                        $stmt_count->store_result();
+                        $rec2 = parent::fetchAll($stmt_count);
+                        if (empty($rec2[0]['cnt'])) {
+                            $stmt_exists_size->bind_param("ii", $args[$i]['size_from'], $rec[$t]['master_id']);
+                            $stmt_exists_size->execute();
+                            $stmt_exists_size->store_result();
+                            $rec3 = parent::fetchAll($stmt_exists_size);
+
+                            // 新しいJANコードで且つ同じサイズが未登録の場合は新規登録
+                            if (empty($rec3[0]['cnt'])) {
+                                $dat = $rec[$t];
+                                $stmt_insert->bind_param("iiiiiss", $dat['master_id'], $dat['itemid'], $dat['size_from'], $args[$i]['amount'], $maker_id, $args[$i]['jancode'], date('Y-m-d H:i:s'));
+                                $maker_id = 1;
+                                $stmt_insert->execute();
+                            }
+                        } else {
+                            $stmt_update->bind_param("iss", $args[$i]['amount'], date('Y-m-d H:i:s'), $args[$i]['jancode']);
+                            $stmt_update->execute();
+                        }
+                    }
+                }
+            }
+            if (empty($err)) {
+                $rs = '更新完了';
+            } else {
+                $rs = $err;
+            }
+
+            // トムスの未更新アイテムを削除
+            $sql = "delete from itemstock where stock_maker=1 and stock_updated<'".$update_time."'";
+            $result = $conn->query($sql);
+            if (!$result) {
+                throw new Exception('initialize');
+            }
+        } catch (Exception $e) {
+            if ($e->getMessage()=='initialize') {
+                $rs = 'Error: Initialization.';
+            } else {
+                $rs = 'Error: Exception.';
+            }
+        } finally {
+            $stmt_count->close();
+            $stmt_update->close();
+            $stmt_choose->close();
+            $stmt_insert->close();
+            $conn->close();
+        }
+
+        return $rs;
+    }
+
+    /**
+     * tomsmasterテーブル更新
+     *
+     * @param array $args
+     * @return void
+     */
+    public function update_toms_master($args)
+    {
+        try {
+            if (empty($args)) {
+                return;
+            }
+
+            $conn = parent::db_connect();
+
+            $update_time = date('Y-m-d H:i:s');
+
+            // 既存の在庫レコードの有無を確認
+            $sql1 = "select count(*) as cnt from tomsmaster where jan_code=?";
+            $stmt_count = $conn->prepare($sql1);
+
+            // 既存の在庫レコードがあれば更新
+            $sql2 = "update tomsmaster set updated_at=? where jan_code=? limit 1";
+            $stmt_update = $conn->prepare($sql2);
+
+            // 新規登録
+            $sql4 = "insert into tomsmaster(tomsid, toms_item_code, toms_item_name, toms_color_code, toms_color_name, toms_size_code, toms_size_name, jan_code, updated_at) values(null,?,?,?,?,?,?,?,?)";
+            $stmt_insert = $conn->prepare($sql4);
+
+            $len = count($args);
+            for ($i = 0; $i < $len; $i++) {
+                $stmt_count->bind_param("s", $args[$i]['jan_code']);
+                $stmt_count->execute();
+                $stmt_count->store_result();
+                $rec = parent::fetchAll($stmt_count);
+
+                if (empty($rec[0]['cnt'])) {
+                    $stmt_insert->bind_param(
+                        "ssssssss",
+                        $$args[$i]['item_code'],
+                        $$args[$i]['item_name'],
+                        $args[$i]['color_code'],
+                        $args[$i]['color_name'],
+                        $args[$i]['size_code'],
+                        $args[$i]['size_name'],
+                        $args[$i]['jan_code'],
+                        date('Y-m-d H:i:s')
+                    );
+                    $stmt_insert->execute();
+                } else {
+                    $stmt_update->bind_param("ss", date('Y-m-d H:i:s'), $args[$i]['jan_code']);
+                    $stmt_update->execute();
+                }
+            }
+
+            // トムスマスターの未更新アイテムを削除
+            $sql = "delete from tomsmaster where updated_at < '".$update_time."'";
+            $result = $conn->query($sql);
+        } finally {
+            $stmt_count->close();
+            $stmt_update->close();
+            $stmt_insert->close();
+            $conn->close();
+        }
+    }
 }
 
 
@@ -159,48 +231,51 @@ mb_language('ja');
 $raw_mail = '';
 /*
 if ( ($stdin=fopen("php://stdin",'r')) == true ){
-	while( !feof($stdin) ){
-		$raw_mail .= fgets($stdin,4096);
-	}
+    while( !feof($stdin) ){
+        $raw_mail .= fgets($stdin,4096);
+    }
 }
 */
-if ( STDIN == true ){		// stdinへのオープン済みストリーム
-	while ( !feof(STDIN) ) {
-		$raw_mail .= fgets(STDIN,4096);
-	}
+if (STDIN == true) {		// stdinへのオープン済みストリーム
+    while (!feof(STDIN)) {
+        $raw_mail .= fgets(STDIN, 4096);
+    }
 }
 
 require_once dirname(__FILE__).'/stock_response.php';
 
 $inst = new TomsStock();
 if (empty($data)) {
-	$args = '不明なメール: '.$decoder->attachments[0]['binary']."\n\n";
+    $args = '不明なメール: '.$decoder->attachments[0]['binary']."\n\n";
 } else {
-	// データベース更新
-	$res = $inst->update_toms_stockdata($data);
-	
-	if (is_array($res)) {
-		$args = "更新完了: ".date('Y-m-d H:i:s')."\n\n";
-		$args .= "未更新アイテム\n";
-		for($i=0; $i<count($res); $i++){
-			$args .= "data: ".$res[$i]."\n";
-		}
-	} else {
-		if (is_array($res)) {
-			$args = "更新完了: ".date('Y-m-d H:i:s')."\n\n";
-			$args .= "未更新アイテム\n";
-			for($i=0; $i<count($res); $i++){
-				$args .= "data: ".$res[$i]."\n";
-			}
-		} else if ($res === '更新完了') {
-			$args = $res.": ".date('Y-m-d H:i:s')."\n\n";
-		} else {
-			$args = $res."\n\n";
-		}
-	}
+    // データベース更新
+    $res = $inst->update_toms_stockdata($data);
+
+    // tomsmaster更新
+    if (!empty($tomsMaster)) {
+        $inst->update_toms_master($tomsMaster);
+    }
+
+    if (is_array($res)) {
+        $args = "更新完了: ".date('Y-m-d H:i:s')."\n\n";
+        $args .= "未更新アイテム\n";
+        for ($i=0; $i<count($res); $i++) {
+            $args .= "data: ".$res[$i]."\n";
+        }
+    } else {
+        if (is_array($res)) {
+            $args = "更新完了: ".date('Y-m-d H:i:s')."\n\n";
+            $args .= "未更新アイテム\n";
+            for ($i=0; $i<count($res); $i++) {
+                $args .= "data: ".$res[$i]."\n";
+            }
+        } elseif ($res === '更新完了') {
+            $args = $res.": ".date('Y-m-d H:i:s')."\n\n";
+        } else {
+            $args = $res."\n\n";
+        }
+    }
 }
 
 // 更新結果を送信
 //$inst->send($args);
-
-?>
